63fdc1e93428a9ab119a8ea1ab445a8fa581491e
KateSt <kateystetsenko@gmail.com>
2025-06-27 07:46:40 +0300

    refactor

--- src/game/Physics.tsx
+++ src/game/Physics.tsx
@@ -1,10 +1,10 @@
 import Matter from 'matter-js';
 
 const Physics =  (entities: any, { time }: any) => {
-    let engine = entities.physics.engine;
-    const MAX_DELTA = 16.667;
-    Matter.Engine.update(engine, Math.min(time.delta, MAX_DELTA));
-    return entities;
+  let engine = entities.physics.engine;
+  const MAX_DELTA = 16.667;
+  Matter.Engine.update(engine, Math.min(time.delta, MAX_DELTA));
+  return entities;
 };
 
 export default Physics;
